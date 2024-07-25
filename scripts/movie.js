eac0567d9d10f6e6fc2eee277f025c33a4009b52
minyeong <minyk0512@gmail.com>
2024-07-26 03:01:48 +0900

    [fix] movie 관련 변수명 수정

--- scripts/movie.js
+++ scripts/movie.js
@@ -4,19 +4,25 @@ const movies = document.querySelector('.movies');
 export function setMovieInfo() {
   // Top10 영화 카드 추가
   topRatedMovies.forEach((movie) => {
-    let testCard = document.createElement('div');
-    testCard.className = 'movieCard';
-    movies.appendChild(testCard);
-    let title = movie.title;
-    let posterImage = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    let ratingScore = movie.vote_average.toFixed(1);
-    let overview = movie.overview;
+    const movieCard = document.createElement('div');
+    movieCard.className = 'movieCard';
+    const id = movie.id;
+    movieCard.id = id;
+    movies.appendChild(movieCard);
 
-    testCard.style.backgroundImage = `url(${posterImage})`;
-    let contentHtml = `
+    const title = movie.title;
+    const posterImage = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const ratingScore = movie.vote_average.toFixed(1);
+    const overview = movie.overview;
+
+    movieCard.style.backgroundImage = `url(${posterImage})`;
+    const contentHtml = `
       <p class="title">${title}</p>
       <p class="ratingScroe">${ratingScore}</p>
     `;
-    testCard.innerHTML = contentHtml;
+
+    movieCard.innerHTML = contentHtml;
   });
 }
+
+function alertID() {}
