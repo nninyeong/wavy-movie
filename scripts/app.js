9de1d370a2f370fa8f3a7f29594dc3f79f08adf3
minyeong <minyk0512@gmail.com>
2024-07-26 10:05:55 +0900

    [refactor] fetch 후 정보 그려내는 것까지 완료하도록 수정

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,3 @@
 import {fetchFromTMDB} from './tmdb.js';
-import {setMovieInfo} from './movie.js';
 
-fetchFromTMDB().then(setMovieInfo);
+fetchFromTMDB();
--- scripts/tmdb.js
+++ scripts/tmdb.js
@@ -1,6 +1,6 @@
 import {TMDB_TOPRATING_AUTH} from './apiKeys.js';
 
-export let topRatedMovies;
+let topRatedMovies;
 const SORT_OPTION = 'vote_average'; // 영화 정렬 기준
 
 const options = {
@@ -24,5 +24,38 @@ export function fetchFromTMDB() {
         })
         .splice(0, 10);
     })
+    .then(setMovieInfo)
     .catch((err) => console.error(err));
+}
+
+const movies = document.querySelector('.movies');
+function setMovieInfo() {
+  // Top10 영화 카드 추가
+  topRatedMovies.forEach((movie) => {
+    const movieCard = document.createElement('div');
+    movieCard.className = 'movieCard';
+    const id = movie.id;
+    movieCard.id = id;
+    movies.appendChild(movieCard);
+
+    const title = movie.title;
+    const posterImage = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const ratingScore = movie.vote_average.toFixed(1);
+    const overview = movie.overview;
+
+    movieCard.style.backgroundImage = `url(${posterImage})`;
+    const contentHtml = `
+      <p class="title">${title}</p>
+      <p class="ratingScroe">${ratingScore}</p>
+    `;
+
+    movieCard.innerHTML = contentHtml;
+    movieCard.addEventListener('click', (event) => {
+      alertID(event);
+    });
+  });
+}
+
+function alertID(event) {
+  alert(`영화 ID: ${event.target.id}`);
 }
