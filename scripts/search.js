f0b15e272865d2d5f5b45642187323cdb65661f8
minyeong <minyk0512@gmail.com>
2024-07-26 16:40:04 +0900

    [update] 타이틀 클릭시 index.html로 이동

--- scripts/search.js
+++ scripts/search.js
@@ -1,11 +1,17 @@
 const searchButton = document.querySelector('.searchButton');
 const searchText = document.querySelector('.searchText');
+const title = document.querySelector('.title');
 
 export function setSearchButtonEvent() {
   searchButton.addEventListener('click', searchMovies);
+  title.addEventListener('click', goIndex);
 }
 
 function searchMovies() {
   const keyword = searchText.value;
   window.location.href = `./searchResult.html?keyword=${encodeURIComponent(keyword)}`;
+}
+
+function goIndex() {
+  window.location.href = `./index.html`;
 }
